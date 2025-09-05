9ef8a71a092f6a8fb70d4dd0cf51a527c8dc0f92
Prince maddheshiya <92073680+Princemaddheshiya@users.noreply.github.com>
2025-09-05 16:09:10 +0530

    Update Frontend.jsx

--- src/components/skills/Frontend.jsx
+++ src/components/skills/Frontend.jsx
@@ -4,27 +4,27 @@ import { HiCheckBadge } from "react-icons/hi2";
 const Frontend = () => {
     return (
         <div className="skills__content">
-            <h3 className="skills__title">Frontend Developer</h3>
+            <h3 className="skills__title">DevOps & Version Control</h3>
             <div className="skills__box">
                 <div className="skills__group">
                     <div className="skills__data">
                         <HiCheckBadge />
                         <div>
-                            <h3 className="skills__name">HTML</h3>
+                            <h3 className="skills__name">Docker</h3>
                             <span className="skills__level">Basic</span>
                         </div>
                     </div>
                     <div className="skills__data">
                         <HiCheckBadge />
                         <div>
-                            <h3 className="skills__name">CSS</h3>
+                            <h3 className="skills__name">Git</h3>
                             <span className="skills__level">Intermediate</span>
                         </div>
                     </div>
                     <div className="skills__data">
                         <HiCheckBadge />
                         <div>
-                            <h3 className="skills__name">Bootstrap</h3>
+                            <h3 className="skills__name">GitHub</h3>
                             <span className="skills__level">Intermediate</span>
                         </div>
                     </div>
@@ -33,22 +33,22 @@ const Frontend = () => {
                     <div className="skills__data">
                         <HiCheckBadge />
                         <div>
-                            <h3 className="skills__name">JavaScript</h3>
+                            <h3 className="skills__name">Grafana</h3>
                             <span className="skills__level">Intermediate</span>
                         </div>
                     </div>
                     <div className="skills__data">
                         <HiCheckBadge />
                         <div>
-                            <h3 className="skills__name">TailwindCSS</h3>
-                            <span className="skills__level">Basic</span>
+                            <h3 className="skills__name">Linux</h3>
+                            <span className="skills__level">Intermediate</span>
                         </div>
                     </div>
                     <div className="skills__data">
                         <HiCheckBadge />
                         <div>
-                            <h3 className="skills__name">React</h3>
-                            <span className="skills__level">Basic</span>
+                            <h3 className="skills__name">Unix</h3>
+                            <span className="skills__level">Intermediate</span>
                         </div>
                     </div>
                 </div>
