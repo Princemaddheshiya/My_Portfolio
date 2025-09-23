9a28018c7917560f703f38a21208fa137afa879d
Prince maddheshiya <92073680+Princemaddheshiya@users.noreply.github.com>
2025-09-23 14:01:47 +0530

    Update Services.jsx

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -12,7 +12,7 @@ const Services = () => {
     return (
         <section className="services section" id="services">
             <h2 className="section__title">Certification</h2>
-            <span className="section__subtitle">Create + Collaborate + Certification</span>
+            <span className="section__subtitle">Create + Collaborate + Experience Certification</span>
 
             <div className="services__container container grid">
                 <div className="services__content">
